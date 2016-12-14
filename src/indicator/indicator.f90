47646034a79ee20b700612a90daf922de6136c9a
iagkrais <krais@iag.uni-stuttgart.de>
2016-12-14 11:19:55 +0100

    Use PP directives to remove calculation of pressure in Persson indicator when not using NSE. Also add some aborts if Indicators that only work with NSE are used with other equation systems and added some doxygen comments.

--- src/indicator/indicator.f90
+++ src/indicator/indicator.f90
@@ -144,11 +144,19 @@ CASE(INDTYPE_JAMESON)
   CALL Abort(__STAMP__, &
       "Jameson indicator only works with FV_ENABLED.")
 #endif
+#if EQNSYSNR != 2 /* NOT NAVIER-STOKES */ 
+  CALL Abort(__STAMP__, &
+      "Jameson indicator only works with Navier-Stokes equations.")
+#endif /* EQNSYSNR != 2 */
 CASE(INDTYPE_DUCROS)
 #if !(PARABOLIC)
   CALL Abort(__STAMP__, &
       "Ducros indicator not available without PARABOLIC!")
 #endif
+#if EQNSYSNR != 2 /* NOT NAVIER-STOKES */ 
+  CALL Abort(__STAMP__, &
+      "Ducros indicator only works with Navier-Stokes equations.")
+#endif /* EQNSYSNR != 2 */
 CASE(INDTYPE_PERSSON)
   ! number of modes to be checked by Persson indicator
   nModes = GETINT('nModes','2')
@@ -169,10 +177,10 @@ SWRITE(UNIT_stdOut,'(A)')' INIT INDICATOR DONE!'
 SWRITE(UNIT_StdOut,'(132("-"))')
 END SUBROUTINE InitIndicator
 
+!==================================================================================================================================
+!> Perform calculation of the indicator.
+!==================================================================================================================================
 SUBROUTINE CalcIndicator(U,t)
-!==================================================================================================================================
-! Perform calculation of the limiter
-!==================================================================================================================================
 ! MODULES
 USE MOD_Globals
 USE MOD_PreProc
@@ -189,10 +197,8 @@ USE MOD_ChangeBasis
 IMPLICIT NONE
 !----------------------------------------------------------------------------------------------------------------------------------
 ! INPUT / OUTPUT VARIABLES
-! U
-REAL,INTENT(INOUT),TARGET :: U(1:PP_nVar,0:PP_N,0:PP_N,0:PP_N,1:nElems)
-REAL,INTENT(IN)           :: t
-! U
+REAL,INTENT(INOUT),TARGET :: U(1:PP_nVar,0:PP_N,0:PP_N,0:PP_N,1:nElems)   !< Solution
+REAL,INTENT(IN)           :: t                                            !< Simulation time
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER                   :: iElem
@@ -261,25 +267,29 @@ END SELECT
 END SUBROUTINE CalcIndicator
 
 
-FUNCTION IndPersson(U) RESULT(IndValue)
 !==================================================================================================================================
 !> Determine, if given a modal representation solution "U_Modal" is oscillating
 !> Indicator value is scaled to \f$\sigma=0 \ldots 1\f$
 !> Suggested by Persson et al.
 !==================================================================================================================================
+FUNCTION IndPersson(U) RESULT(IndValue)
 USE MOD_PreProc
 USE MOD_Indicator_Vars,ONLY:nModes,IndVar
 USE MOD_Interpolation_Vars, ONLY:sVdm_Leg
+#if EQNSYSNR == 2 /* NAVIER-STOKES */ 
 USE MOD_EOS_Vars
+#endif /* NAVIER-STOKES */
 IMPLICIT NONE
 !----------------------------------------------------------------------------------------------------------------------------------
 ! INPUT/OUTPUT VARIABLES
-REAL,INTENT(IN)    :: U(PP_nVar,0:PP_N,0:PP_N,0:PP_N)
-REAL               :: IndValue
+REAL,INTENT(IN)    :: U(PP_nVar,0:PP_N,0:PP_N,0:PP_N)           !< Solution
+REAL               :: IndValue                                  !< Value of the indicator (Return Value)
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER                              :: iDeg,i,j,k,l
+#if EQNSYSNR == 2 /* NAVIER-STOKES */ 
 REAL                                 :: UE(1:PP_2Var)
+#endif /* NAVIER-STOKES */
 REAL,DIMENSION(0:PP_N,0:PP_N,0:PP_N) :: U_loc
 REAL,DIMENSION(0:PP_N,0:PP_N,0:PP_N) :: U_Xi
 REAL,DIMENSION(0:PP_N,0:PP_N,0:PP_N) :: U_Eta
@@ -288,6 +298,7 @@ REAL,DIMENSION(0:PP_N,0:PP_N,0:PP_N) :: U_Modal
 SELECT CASE (IndVar)
 CASE(1:PP_nVar)
   U_loc = U(IndVar,:,:,:)
+#if EQNSYSNR == 2 /* NAVIER-STOKES */ 
 CASE(6)
   DO k=0,PP_N; DO j=0,PP_N; DO i=0,PP_N
     UE(CONS)=U(:,i,j,k)
@@ -295,6 +306,7 @@ CASE(6)
     UE(VELV)=VELOCITY_HE(UE)
     U_loc(i,j,k)=PRESSURE_HE(UE)
   END DO; END DO; END DO! i,j,k=0,PP_N
+#endif /* NAVIER-STOKES */
 END SELECT
 
 ! Transform nodal solution to a modal representation
@@ -332,6 +344,9 @@ END FUNCTION IndPersson
 
 #if EQNSYSNR == 2 /* NAVIER-STOKES */ 
 #if PARABOLIC
+!==================================================================================================================================
+!> Indicator by Ducros.
+!==================================================================================================================================
 FUNCTION DucrosIndicator(gradUx, gradUy, gradUz) RESULT(IndValue) 
 USE MOD_PreProc
 USE MOD_Mesh_Vars          ,ONLY: nElems,sJ
@@ -339,11 +354,13 @@ USE MOD_Analyze_Vars       ,ONLY: wGPVol
 #if FV_ENABLED
 USE MOD_FV_Vars            ,ONLY: FV_Elems
 #endif
+IMPLICIT NONE
+!----------------------------------------------------------------------------------------------------------------------------------
 ! INPUT / OUTPUT VARIABLES 
-REAL,INTENT(IN)    :: gradUx(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)
-REAL,INTENT(IN)    :: gradUy(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)
-REAL,INTENT(IN)    :: gradUz(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)
-REAL               :: IndValue(1:nElems)
+REAL,INTENT(IN)    :: gradUx(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)   !< Gradients in x-direction
+REAL,INTENT(IN)    :: gradUy(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)   !< Gradients in x-direction
+REAL,INTENT(IN)    :: gradUz(PP_nVarPrim,0:PP_N,0:PP_N,0:PP_N,1:nElems)   !< Gradients in x-direction
+REAL               :: IndValue(1:nElems)                                  !< Value of the indicator (Return Value)
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER :: i,j,k,iElem
@@ -373,6 +390,9 @@ END FUNCTION DucrosIndicator
 #endif /* PARABOLIC */
 
 #if FV_ENABLED
+!==================================================================================================================================
+!> Indicator by Jameson.
+!==================================================================================================================================
 FUNCTION JamesonIndicator(U) RESULT(IndValue)
 USE MOD_PreProc
 USE MOD_Globals
@@ -393,10 +413,11 @@ USE MOD_MPI                ,ONLY: StartReceiveMPIData,StartSendMPIData,FinishExc
 #endif
 USE MOD_FillMortar1        ,ONLY: U_Mortar1,Flux_Mortar1
 USE MOD_FV_Vars            ,ONLY: FV_Elems,FV_Elems_master,FV_Elems_slave
+!----------------------------------------------------------------------------------------------------------------------------------
 IMPLICIT NONE
 ! INPUT / OUTPUT VARIABLES 
-REAL,INTENT(IN)           :: U(PP_nVar,0:PP_N,0:PP_N,0:PP_N,nElems)
-REAL                      :: IndValue(1:nElems)
+REAL,INTENT(IN)           :: U(PP_nVar,0:PP_N,0:PP_N,0:PP_N,nElems)              !< Solution
+REAL                      :: IndValue(1:nElems)                                  !< Value of the indicator (Return Value)
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 REAL                      :: ElemVol,IntegrationWeight
