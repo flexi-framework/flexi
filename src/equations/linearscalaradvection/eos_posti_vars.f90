6d5139c7655c7e10d166ac1a32e47023208fdab5
Thomas Bolemann <thomas.bolemann@iag.uni-stuttgart.de>
2017-01-17 16:34:49 +0100

    Add EOS_Posti dummy routines for linearscalaradvection in Posti.

---
+++ src/equations/linearscalaradvection/eos_posti_vars.f90
@@ -0,0 +1,50 @@
+!=================================================================================================================================
+! Copyright (c) 2016  Prof. Claus-Dieter Munz 
+! This file is part of FLEXI, a high-order accurate framework for numerically solving PDEs with discontinuous Galerkin methods.
+! For more information see https://www.flexi-project.org and https://nrg.iag.uni-stuttgart.de/
+!
+! FLEXI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+! as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!
+! FLEXI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+! of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
+!
+! You should have received a copy of the GNU General Public License along with FLEXI. If not, see <http://www.gnu.org/licenses/>.
+!=================================================================================================================================
+#include "flexi.h"
+
+!==================================================================================================================================
+! Posti related variables
+!==================================================================================================================================
+MODULE MOD_EOS_Posti_Vars
+! MODULES
+IMPLICIT NONE
+PUBLIC
+SAVE
+
+INTEGER,PARAMETER :: nVarTotalEOS=1
+! W
+! i
+! t
+! h
+! D
+! G
+! O
+! p
+! e S
+! r c
+! a a
+! t l
+! o a
+! r r
+INTEGER,DIMENSION(1:nVarTotalEOS,0:nVarTotalEOS),PARAMETER :: DepTableEOS = TRANSPOSE(RESHAPE(&
+(/&
+  0,1 & !1  Scalar
+/),(/nVarTotalEOS+1,nVarTotalEOS/)))
+
+CHARACTER(LEN=255),DIMENSION(nVarTotalEOS),PARAMETER :: DepNames = &
+(/ CHARACTER(LEN=255) :: &
+"Scalar"                 & !1
+/)
+
+END MODULE MOD_EOS_Posti_Vars
