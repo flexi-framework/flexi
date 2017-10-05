9be9a229e4e69e8e18b86ff35172ff5eb3d0ad02
Thomas Bolemann <thomas.bolemann@iag.uni-stuttgart.de>
2017-10-05 14:48:50 +0200

    Add comments. Exclude inner mortars in NormVec routine.

--- src/dg/br1/br1_fillflux.f90
+++ src/dg/br1/br1_fillflux.f90
@@ -43,12 +43,13 @@ PUBLIC::Lifting_FillFlux,Lifting_FillFlux_BC,Lifting_FillFlux_NormVec
 CONTAINS
 
 !==================================================================================================================================
-!> \brief Computes the BR1 surface fluxes in direction "dir" used to compute the gradient in direction "dir",
+!> \brief Computes the untransformed BR1 surface fluxes used to compute the gradient in direction "dir",
 !> surfelem contribution is considered as well.
 !>
-!> Fills the interior surface fluxes for the BR1 scheme. Is called on a per direction basis. The numerical flux in the
+!> Fills the untransformed interior surface fluxes for the BR1 scheme. The numerical flux in the
 !> BR1 lifting is simply taken as the arithmetic mean of the solution. 
-!> The physical flux is  multiplied by the normal vector and the surface element contribution to transform into reference space,
+!> The physical flux will be multiplied by the surface element contribution and in a later routine by the normal vector to
+!> transform into reference space,
 !> see e.g. "Explicit discontinuous Galerkin methods for unsteady problems" (Hindenlang et al. 2012) for details.
 !> For the strong form, in the surface integral the inner solution is substracted form the numerical flux. Since the numerical
 !> flux is \f$ \frac{1}{2} (U^+ + U^-) \f$ and the inner solution is simply \f$ U^- \f$ for the master side
@@ -62,11 +63,10 @@ SUBROUTINE Lifting_FillFlux(UPrimface_master,UPrimface_slave,Flux,doMPISides)
 USE MOD_Globals
 USE MOD_PreProc
 USE MOD_Lifting_Vars,    ONLY: doWeakLifting
-USE MOD_Mesh_Vars,       ONLY: SurfElem,NormVec
+USE MOD_Mesh_Vars,       ONLY: SurfElem
 USE MOD_Mesh_Vars,       ONLY: nSides
 USE MOD_Mesh_Vars,       ONLY: firstInnerSide,   lastInnerSide
 USE MOD_Mesh_Vars,       ONLY: firstMPISide_MINE,lastMPISide_MINE
-USE MOD_Mesh_Vars,       ONLY: firstMPISide_YOUR,lastMPISide_YOUR
 #if FV_ENABLED
 USE MOD_FV_Vars         ,ONLY: FV_Elems_Sum,FV_sVdm
 USE MOD_ChangeBasisByDim,ONLY: ChangeBasisSurf
@@ -78,7 +78,7 @@ LOGICAL,INTENT(IN) :: doMPISides                                          !< = .
                                                                           !< =.FALSE. InnerSides
 REAL,INTENT(INOUT) :: UPrimface_master(PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Solution on master sides
 REAL,INTENT(INOUT) :: UPrimface_slave( PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Solution on slave sides
-REAL,INTENT(INOUT) :: Flux(            PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Lifting-Flux
+REAL,INTENT(INOUT) :: Flux(            PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Untransformed lifting flux
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER            :: SideID,p,q,firstSideID,lastSideID,sig
@@ -123,20 +123,13 @@ DO SideID=firstSideID,lastSideID
 #endif
 
   ! BR1 uses arithmetic mean value of states for the Riemann flux
-  ! NormVec is applied in separate routine
+  ! Transformation with NormVec is applied in separate routine
   DO q=0,PP_NZ
     DO p=0,PP_N
       Flux(:,p,q,SideID)=0.5*Flux(:,p,q,SideID)*SurfElem(p,q,0,SideID)
     END DO
   END DO
 END DO ! SideID
-
-!IF(myrank.EQ.0)THEN
-!IPWRITE(*,*) normvec(3,0,0,0,firstMPISide_MINE:lastMPISide_MINE)
-!else
-!IPWRITE(*,*) normvec(3,0,0,0,firstMPISide_YOUR:lastMPISide_YOUR)
-!endif
-!STOP
 
 END SUBROUTINE Lifting_FillFlux
 
@@ -145,14 +138,13 @@ END SUBROUTINE Lifting_FillFlux
 !==================================================================================================================================
 !> \brief Computes the BR1 surface fluxes for boundary conditions, surfelem contribution is considered as well.
 !> 
-!> Fills the boundary surface fluxes for the BR1 scheme. Is called for all directions at once. The numerical flux in the
+!> Fills the Untransformed boundary surface fluxes for the BR1 scheme. The numerical flux in the
 !> BR1 lifting is simply taken as the arithmetic mean of the solution. 
 !> This routine calls the equation system dependant routine Lifting_GetBoundaryFlux which will fill the flux depending on the
 !> boundary condition that has to be applied. The Lifting_GetBoundaryFlux routine will also differentiate between weak and
 !> strong form and already multiply the flux by the surface element.
 !> 
-!> The returned flux is multiplied by the normal vector to transform into reference space (in combination with the 
-!> surface element).
+!> The multiplication by the normal vector is done in a separate routine.
 !> 
 !> The flux is filled for the master side, the contribution for the slave side (which is different because the inner solution
 !> is equal to \f$ U^+ \f$) is taken into account in the SurfInt routine.
@@ -170,49 +162,42 @@ IMPLICIT NONE
 ! INPUT/OUTPUT VARIABLES
 REAL,INTENT(IN)    :: t                                               !< Current solution time
 REAL,INTENT(IN)    :: UPrim_master(PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Primitive solution from the inside
-REAL,INTENT(INOUT) :: Flux(        PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Lifting boundary flux in x direction
+REAL,INTENT(INOUT) :: Flux(        PP_nVarPrim,0:PP_N,0:PP_NZ,nSides) !< Untransformed lifting boundary flux
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER            :: SideID
 !==================================================================================================================================
-! fill flux for boundary sides, only compute boundary flux once
+! compute untransformed flux for boundary sides, normvec is applied later
 DO SideID=1,nBCSides
   IF (FV_Elems_master(SideID).GT.0) CYCLE
   CALL Lifting_GetBoundaryFlux(SideID,t,UPrim_master(:,:,:,SideID),Flux(:,:,:,SideID),&
        NormVec(:,:,:,0,SideID),TangVec1(:,:,:,0,SideID),TangVec2(:,:,:,0,SideID),Face_xGP(:,:,:,0,SideID),SurfElem(:,:,0,SideID))
 END DO
-
 END SUBROUTINE Lifting_FillFlux_BC
 
 
 !==================================================================================================================================
-!> \brief Computes the BR1 surface fluxes for boundary conditions, surfelem contribution is considered as well.
+!> \brief Multiplies the untransformed flux (already weighted by the surface element) by the normal vector to complete the
+!>        transformation into reference space.
 !> 
-!> Fills the boundary surface fluxes for the BR1 scheme. Is called for all directions at once. The numerical flux in the
-!> BR1 lifting is simply taken as the arithmetic mean of the solution. 
-!> This routine calls the equation system dependant routine Lifting_GetBoundaryFlux which will fill the flux depending on the
-!> boundary condition that has to be applied. The Lifting_GetBoundaryFlux routine will also differentiate between weak and
-!> strong form and already multiply the flux by the surface element.
-!> 
-!> The returned flux is multiplied by the normal vector to transform into reference space (in combination with the 
-!> surface element).
-!> 
-!> The flux is filled for the master side, the contribution for the slave side (which is different because the inner solution
-!> is equal to \f$ U^+ \f$) is taken into account in the SurfInt routine.
+!> The untransformed fluxes is multiplied for each gradient direction by the corresponding normal vector,
+!> which is the second step of the transformation. We end up with the lifting fluxes for X/Y/Z direction.
 !==================================================================================================================================
 SUBROUTINE Lifting_FillFlux_NormVec(Flux,FluxX,FluxY,FluxZ,doMPISides)
 ! MODULES
 USE MOD_PreProc
-USE MOD_Mesh_Vars,       ONLY: NormVec,nSides,nBCSides
+USE MOD_Mesh_Vars,       ONLY: NormVec,nSides,MortarType
 USE MOD_Mesh_Vars,       ONLY: firstMPISide_YOUR,lastMPISide_MINE,lastMPISide_YOUR
 USE MOD_GetBoundaryFlux, ONLY: Lifting_GetBoundaryFlux
 #if FV_ENABLED  
 USE MOD_FV_Vars,         ONLY: FV_Elems_master,FV_Elems_Sum
+USE MOD_Mesh_Vars,       ONLY: nBCSides
 #endif
 IMPLICIT NONE
 !----------------------------------------------------------------------------------------------------------------------------------
 ! INPUT/OUTPUT VARIABLES
-LOGICAL,INTENT(IN) :: doMPISides                               !< = .TRUE. only MINE MPISides are filled
+LOGICAL,INTENT(IN) :: doMPISides                                 !< = .TRUE. MPI_YOUR
+                                                                 !< =.FALSE. BC+Inner+MPI_Mine
 REAL,INTENT(IN)    :: Flux( PP_nVarPrim,0:PP_N,0:PP_NZ,1:nSides) !< Untransformed Lifting boundary flux
 REAL,INTENT(INOUT) :: FluxX(PP_nVarPrim,0:PP_N,0:PP_NZ,1:nSides) !< Lifting boundary flux in x direction
 REAL,INTENT(INOUT) :: FluxY(PP_nVarPrim,0:PP_N,0:PP_NZ,1:nSides) !< Lifting boundary flux in y direction
@@ -223,7 +208,7 @@ INTEGER            :: firstSideID,lastSideID,SideID,p,q
 !==================================================================================================================================
 ! fill flux for sides ranging between firstSideID and lastSideID using Riemann solver
 IF(doMPISides)THEN
-  ! apply normal vector to YOUR and MPI mortars fluxes
+  ! apply normal vector to YOUR
   firstSideID = firstMPISide_YOUR
    lastSideID =  lastMPISide_YOUR
 ELSE
@@ -237,6 +222,7 @@ DO SideID=firstSideID,lastSideID
   IF (FV_Elems_master(SideID).GT.0.AND.SideID.LE.nBCSides) CYCLE ! FV BC already filled
   IF (FV_Elems_Sum(SideID).EQ.3)                           CYCLE ! FV/FV already filled
 #endif
+  IF(MortarType(1,SideID).LE.0)                            CYCLE ! no big mortars
   DO q=0,PP_NZ; DO p=0,PP_N
     FluxX(:,p,q,SideID)=Flux(:,p,q,SideID)*NormVec(1,p,q,0,SideID)
     FluxY(:,p,q,SideID)=Flux(:,p,q,SideID)*NormVec(2,p,q,0,SideID)
