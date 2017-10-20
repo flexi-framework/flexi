767747a95cf34171abb39768a2d5a6a2a86cba1d
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2017-10-20 17:51:43 +0200

    reggie2.0: added comments for all analyze routines

--- regressioncheck/analysis.py
+++ regressioncheck/analysis.py
@@ -79,14 +79,33 @@ def getAnalyzes(path, example) :
 class Analyze_L2() :
     def __init__(self, L2_tolerance) :
         self.L2_tolerance = L2_tolerance
+
     def perform(self,runs) :
+
+        # General workflow:
+        # 1.  Iterate over all runs
+        # 1.1   read L2 errors from 'std.out' file
+        # 1.2   if one L2 errors is larger than the tolerance -> fail
+        # 1.3   append info for summary of errors
+        # 1.4   set analyzes to fail
+
+        # 1.  Iterate over all runs
         for run in runs :
+            
+            # 1.1   read L2 errors from 'std.out' file
             L2_errors = np.array(analyze_functions.get_last_L2_error(run.stdout))
+            
+            # 1.2   if one L2 errors is larger than the tolerance -> fail
             if (L2_errors > self.L2_tolerance).any() :
                 print tools.red("analysis failed: L2 error >"+str(self.L2_tolerance))
+                
+                # 1.3   append info for summary of errors
                 run.analyze_results.append("analysis failed: L2 error >"+str(self.L2_tolerance))
                 #global_errors+=1
+
+                # 1.4   set analyzes to fail
                 run.analyze_successful=False
+
     def __str__(self) :
         return "perform L2 error comparison with a pre-defined tolerance"
 
@@ -99,47 +118,69 @@ class Analyze_Convtest_h() :
         self.rate = rate
 
     def perform(self,runs) :
-        # number of successful runs must be euqal the number of supplied cells
-        if len(self.cells) == len(runs) :
-            p = float(runs[0].parameters.get('N',-1)) # get polynomial degree
 
-            # get L2 errors of all runs and create np.array
+        # General workflow:
+        # 1.  check if number of successful runs is euqal the number of supplied cells
+        # 1.1   read the polynomial degree from the first run -> must not change!
+        # 1.2   get L2 errors of all runs and create np.array
+        # 1.3   get number of variables from L2 error array
+        # 1.4   determine order of convergence between two runs
+        # 1.5   determine success rate by comparing the relative convergence error with a tolerance
+        # 1.6   compare success rate with pre-defined rate
+        # 1.7     interate over all runs
+        # 1.7.1   add failed info if success rate is not reached to all runs
+        # 1.7.2   set analyzes to fail if success rate is not reached for all runs
+
+        # 1.  check if number of successful runs is euqal the number of supplied cells
+        if len(self.cells) == len(runs) :
+
+            # 1.1   read the polynomial degree from the first run -> must not change!
+            p = float(runs[0].parameters.get('N',-1))
+
+            # 1.2   get L2 errors of all runs and create np.array
             all_L2_errors = np.array([analyze_functions.get_last_L2_error(run.stdout) for \
                     run in runs])
             all_L2_errors = np.transpose(all_L2_errors)
 
-            # get number of variables from L2 error array
+            # 1.3   get number of variables from L2 error array
             nVar = len(all_L2_errors)
-            
             print tools.blue("L2 errors nVar="+str(nVar))
             print all_L2_errors
 
-            # determine order of convergence between two runs
+            # 1.4   determine order of convergence between two runs
             all_L2_order = \
                     np.array([analyze_functions.calcOrder_h(self.cells,all_L2_errors[x]) for \
                     x in range(nVar)])
-
             print tools.blue("L2 orders for nVar="+str(nVar))
             print all_L2_order
 
             # determine average convergence rate
             mean = [np.mean(all_L2_order[x]) for x in range(nVar)]
             
-            # determine success rate by comparing the relative convergence error with a tolerance
+            # 1.5   determine success rate by comparing the relative convergence error with a tolerance
             print "relative order error ",[abs(mean[x]/(3+1)-1) for x in range(nVar)]
             success = [abs(mean[x]/(p+1)-1) < self.tolerance for x in range(nVar)]
             print "success convergence: ",success
+
+            # 1.6   compare success rate with pre-defined rate, fails if not reached
             if float(sum(success))/nVar >= self.rate :
                 print tools.blue("h-convergence successful")
             else :
                 print tools.red("h-convergence failed"+"\n"+\
                         "success rate="+str(float(sum(success))/nVar)+\
                         " tolerance rate="+str(self.rate))
+
+                # 1.7     interate over all runs
                 for run in runs :
+
+                    # 1.6.1   add failed info if success rate is not reached to all runs
                     run.analyze_results.append("analysis failed: h-convergence "\
                             +str(success))
-                    #global_errors+=1
+
+                    # 1.6.2   set analyzes to fail if success rate is not reached for all runs
                     run.analyze_successful=False
+
+                    #global_errors+=1
 
         else :
             print "cannot perform conv test, because number of successful runs must equal the number of cells"
@@ -156,31 +197,45 @@ class Analyze_Convtest_p() :
         self.rate = rate
 
     def perform(self,runs) :
+
+        # General workflow:
+        # 1.  read the polynomial degree  for all runs
+        # 2.  check if number of successful runs must be euqal the number of supplied cells
+        # 2.2   get L2 errors of all runs and create np.array
+        # 2.3   get number of variables from L2 error array
+        # 2.4   determine order of convergence between two runs
+        # 2.5   check if the order of convergence is always increasing with increasing polynomial degree
+        # 2.6   determine success rate by comparing the relative convergence error with a tolerance
+        # 2.7   compare success rate with pre-defined rate, fails if not reached 
+        # 2.8   interate over all runs
+        # 2.8.1   add failed info if success rate is not reached to all runs
+        # 2.8.1   set analyzes to fail if success rate is not reached for all runs
+
+        # 1.  read the polynomial degree  for all runs
         p = [float(run.parameters.get('N',-1)) for run in runs] # get polynomial degree
-        # number of successful runs must be euqal the number of supplied cells
+
+        # 2   check if number of successful runs must be euqal the number of supplied cells
         if len(p) == len(runs) :
-            # get L2 errors of all runs and create np.array
+
+            # 2.2   get L2 errors of all runs and create np.array
             all_L2_errors = np.array([analyze_functions.get_last_L2_error(run.stdout) for \
                     run in runs])
             all_L2_errors = np.transpose(all_L2_errors)
 
-            # get number of variables from L2 error array
+            # 2.3   get number of variables from L2 error array
             nVar = len(all_L2_errors)
             
             print tools.blue("L2 errors nVar="+str(nVar))
             print all_L2_errors
 
-            # determine order of convergence between two runs
+            # 2.4   determine order of convergence between two runs
             all_L2_order = \
                     np.array([analyze_functions.calcOrder_p(p,all_L2_errors[x]) for \
                     x in range(nVar)])
-
             print tools.blue("L2 orders for nVar="+str(nVar))
             print all_L2_order
 
-            # determine average convergence rate
-            mean = [np.mean(all_L2_order[x]) for x in range(nVar)]
-
+            # 2.5   check if the order of convergence is always increasing with increasing polynomial degree
             increasing = []
             for j in range(nVar) :
                 increasing_run = []
@@ -188,23 +243,31 @@ class Analyze_Convtest_p() :
                     increasing_run.append(p[i]>p[i-1])
                 #print increasing_run
                 increasing.append(all(increasing_run))
-
             print tools.blue("Increasing order of convergence")
             print tools.blue(str(increasing))
             
-            # determine success rate by comparing the relative convergence error with a tolerance
+            # 2.6   determine success rate by comparing the relative convergence error with a tolerance
             success = [increasing[x] for x in range(nVar)]
             print "success convergence: ",success
+
+            # 2.7   compare success rate with pre-defined rate, fails if not reached
             if float(sum(success))/nVar >= self.rate :
                 print tools.blue("p-convergence successful")
             else :
                 print tools.red("p-convergence failed"+"\n"+\
                         "success rate="+str(float(sum(success))/nVar)+\
                         " tolerance rate="+str(self.rate))
+
+                # 2.8     interate over all runs
                 for run in runs :
+
+                    # 2.8.1   add failed info if success rate is not reached to all runs
                     run.analyze_results.append("analysis failed: p-convergence "+str(success))
-                    #global_errors+=1
+
+                    # 2.8.2   set analyzes to fail if success rate is not reached for all runs
                     run.analyze_successful=False
+
+                    #global_errors+=1
         else :
             print "cannot perform conv test, because number of successful runs must equal the number of polynomial degrees"
             print "length(runs) ",len(runs)
@@ -228,26 +291,42 @@ class Analyze_h5diff(Loop) :
         print self.file
         print self.name
 
+        # General workflow:
+        # 1.  iterate over all runs
+        # 1.2   select relative or absolute comparison
+        # 1.2   set directory in which the program is executed
+        # 1.3   execute the command 'cmd' = 'h5diff -r --XXX [number] ref_file file DataArray'
+        # 1.4   if the comman 'cmd' return a code != 0, set failed
+        # 1.4.1   add failed info if return a code != 0 to run
+        # 1.4.2   set analyzes to fail if return a code != 0
+
+        # 1.  iterate over all runs
         for run in runs :
             h5diff = "/opt/hdf5/1.8.18/bin/h5diff"
 
-            # select relative or absolute comparison
+            # 1.2   select relative or absolute comparison
             if 1 == 1 :
                 cmd = [h5diff,"-r","--delta","1e-5",str(self.reference_file),str(self.file),str(self.name)]
             else :
                 cmd = [h5diff,"-r","--relative","1e-5",str(self.reference_file),str(self.file),str(self.name)]
             print "Running ["," ".join(cmd),"]",
 
-            # set directory in which the program is executed
+            # 1.2   set directory in which the program is executed
             self.target_directory = run.target_directory
 
-            # execute the command 'cmd'
+            # 1.3   execute the command 'cmd' = 'h5diff -r --XXX [number] ref_file file DataArray'
             self.execute_cmd(cmd) # run the code
 
+            # 1.4   if the comman 'cmd' return a code != 0, set failed
             if self.return_code != 0 :
+
+                # 1.4.1   add failed info if return a code != 0 to run
                 run.analyze_results.append("h5diff failed")
-                #global_errors+=1
+
+                # 1.4.2   set analyzes to fail if return a code != 0
                 run.analyze_successful=False
+
+                #global_errors+=1
 
     def __str__(self) :
         return "perform h5diff between two files"
