da6fb38801c7b76ad89c4feed0572d6284646ca0
iagkrais <krais@iag.uni-stuttgart.de>
2018-12-17 17:13:24 +0100

    Recordpoints: Add licence header

--- posti/recordpoints/visualize/rpset/rpset_vars.f90
+++ posti/recordpoints/visualize/rpset/rpset_vars.f90
@@ -1,3 +1,16 @@
+!=================================================================================================================================
+! Copyright (c) 2010-2016  Prof. Claus-Dieter Munz
+! This file is part of FLEXI, a high-order accurate framework for numerically solving PDEs with discontinuous Galerkin methods.
+! For more information see https://www.flexi-project.org and https://nrg.iag.uni-stuttgart.de/
+!
+! FLEXI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+! as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!
+! FLEXI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+! of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
+!
+! You should have received a copy of the GNU General Public License along with FLEXI. If not, see <http://www.gnu.org/licenses/>.
+!=================================================================================================================================
 !===================================================================================================================================
 !> Contains global variables used for/by the RPSet
 !===================================================================================================================================
