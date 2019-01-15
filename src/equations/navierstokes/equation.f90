6db2cc2ed5839d3b879bdd32e490d49116bb67c2
thomas <iagtkuhn@iagpc151.iag.uni-stuttgart.de>
2019-01-15 11:12:11 +0100

    merge bug: already in define parameters eddy visc

--- src/equations/navierstokes/equation.f90
+++ src/equations/navierstokes/equation.f90
@@ -65,11 +65,6 @@ CALL prms%CreateRealArrayOption('RefState',     "State(s) in primitive variables
 CALL prms%CreateStringOption(   'BCStateFile',  "File containing the reference solution on the boundary to be used as BC.")
 
 CALL DefineParametersRiemann()
-#if EDDYVISCOSITY
-CALL prms%CreateIntFromStringOption(   'eddyViscType', "(0) none: No eddy viscosity, (1) Smagorinsky",'none')
-CALL addStrListEntry('eddyViscType','none',0)
-CALL addStrListEntry('eddyViscType','smagorinsky',1)
-#endif
 #ifdef SPLIT_DG
 CALL DefineParametersSplitDG()
 #endif /*SPLIT_DG*/
