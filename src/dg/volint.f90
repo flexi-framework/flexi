bd073e6f51c2b2cd1b8d60bbc13d6696e911af66
iagkrais <krais@iag.uni-stuttgart.de>
2019-07-15 14:21:49 +0200

    DG/Volint: Fix the inconsistent comment, that we don't need to nullify Ut, just before Ut gets nullified... For this, change the FV and DG volints in a way that the first contributions get written directly and not added to a Ut==0.

--- src/dg/volint.f90
+++ src/dg/volint.f90
@@ -100,7 +100,13 @@ DO iElem=1,nElems
                                      Metrics_hTilde(:,:,:,:,iElem,0))
 
   DO k=0,PP_NZ; DO j=0,PP_N; DO i=0,PP_N
-    DO l=0,PP_N
+      ! Update the time derivative with the spatial derivatives of the transformed fluxes
+      Ut(:,i,j,k,iElem) =                     D_Hat_T(0,i)*f(:,0,j,k) + &
+#if PP_dim==3
+                                              D_Hat_T(0,k)*h(:,i,j,0) + &
+#endif
+                                              D_Hat_T(0,j)*g(:,i,0,k)
+    DO l=1,PP_N
       ! Update the time derivative with the spatial derivatives of the transformed fluxes
       Ut(:,i,j,k,iElem) = Ut(:,i,j,k,iElem) + D_Hat_T(l,i)*f(:,l,j,k) + &
 #if PP_dim==3
@@ -176,6 +182,29 @@ DO iElem=1,nElems
 #endif
 
   DO k=0,PP_NZ; DO j=0,PP_N; DO i=0,PP_N
+    ! consistency: if both points for flux evaluation are the same the standard
+    ! euler fluxes are retained.
+#if PARABOLIC
+    Ut(:,i,j,k,iElem) = DVolSurf(i,i)*f_c(:,i,j,k) + &
+                        DVolSurf(j,j)*g_c(:,i,j,k) + &
+#if PP_dim==3
+                        DVolSurf(k,k)*h_c(:,i,j,k) + &
+#endif /*PP_dim==3*/
+                        D_Hat_T(i,i)*fv(:,i,j,k)   + &
+                        D_Hat_T(j,j)*gv(:,i,j,k)   + &
+#if PP_dim==3
+                        D_Hat_T(k,k)*hv(:,i,j,k)
+#else
+                        0.
+#endif /*PP_dim==3*/
+#else
+    Ut(:,i,j,k,iElem) = DVolSurf(i,i)*f_c(:,i,j,k) + &
+#if PP_dim==3
+                        DVolSurf(j,j)*h_c(:,i,j,k) + &
+#endif /*PP_dim==3*/
+                        DVolSurf(k,k)*g_c(:,i,j,k)
+#endif /*PARABOLIC*/
+
 
     DO l=i+1,PP_N
        ! compute split flux in x-direction
@@ -232,29 +261,6 @@ DO iElem=1,nElems
 #endif /*PARABOLIC*/
     END DO ! l
 #endif /*PP_dim==3*/
-
-    ! consistency: if both poitns for flux evaluation are the same the standard
-    ! euler fluxes are retained
-#if PARABOLIC
-    Ut(:,i,j,k,iElem) = Ut(:,i,j,k,iElem) + DVolSurf(i,i)*f_c(:,i,j,k) + &
-                                            DVolSurf(j,j)*g_c(:,i,j,k) + &
-#if PP_dim==3
-                                            DVolSurf(k,k)*h_c(:,i,j,k) + &
-#endif /*PP_dim==3*/
-                                            D_Hat_T(i,i)*fv(:,i,j,k)   + &
-                                            D_Hat_T(j,j)*gv(:,i,j,k)   + &
-#if PP_dim==3
-                                            D_Hat_T(k,k)*hv(:,i,j,k)
-#else
-                                            0.
-#endif /*PP_dim==3*/
-#else
-    Ut(:,i,j,k,iElem) = Ut(:,i,j,k,iElem) + DVolSurf(i,i)*f_c(:,i,j,k) + &
-#if PP_dim==3
-                                            DVolSurf(j,j)*h_c(:,i,j,k) + &
-#endif /*PP_dim==3*/
-                                            DVolSurf(k,k)*g_c(:,i,j,k)
-#endif /*PARABOLIC*/
 
   END DO; END DO; END DO !i,j,k
 END DO ! iElem
