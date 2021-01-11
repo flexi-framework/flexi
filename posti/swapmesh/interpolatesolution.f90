014c9bef55f3fd251b644ff80a84816ecbf98d57
Marius Kurz <marius.kurz@iag.uni-stuttgart.de>
2021-01-11 13:06:21 +0100

    2D version of swapmesh tool. If compiled in 2D, only 2D calculations will be done. Can read and write true 2D data or data expanded to 3D.

--- posti/swapmesh/interpolatesolution.f90
+++ posti/swapmesh/interpolatesolution.f90
@@ -49,7 +49,7 @@ USE MOD_SwapMesh_Vars,     ONLY: Vdm_CLNInter_GPNNew
 USE MOD_SwapMesh_Vars,     ONLY: UOld,xiInter,InterToElem,nElemsNew,IPDone
 USE MOD_SwapMesh_Vars,     ONLY: Elem_IJK,ExtrudeTo3D,ExtrudeK
 USE MOD_DG_Vars,           ONLY: U
-USE MOD_ChangeBasis,       ONLY: ChangeBasis3D
+USE MOD_ChangeBasisByDim,  ONLY: ChangeBasisVolume
 #if USE_OPENMP
 USE OMP_Lib,               ONLY: OMP_GET_WTIME
 #endif
@@ -64,10 +64,12 @@ IMPLICIT NONE
 INTEGER                       :: i,j,k    ! GPs in old element
 INTEGER                       :: ii,jj,kk ! GPs in new element
 INTEGER                       :: iElemOld,iElemNew
-REAL                          :: Utmp(1:nVar_State,0:NInter,0:NInter,0:NInter)
-REAL                          :: L_xi(  0:NState,0:NInter,0:NInter,0:NInter)
-REAL                          :: L_eta( 0:NState,0:NInter,0:NInter,0:NInter)
-REAL                          :: L_zeta(0:NState,0:NInter,0:NInter,0:NInter)
+REAL                          :: Utmp(1:nVar_State,0:NInter,0:NInter,0:ZDIM(NInter))
+REAL                          :: L_xi(    0:NState,0:NInter,0:NInter,0:ZDIM(NInter))
+REAL                          :: L_eta(   0:NState,0:NInter,0:NInter,0:ZDIM(NInter))
+#if PP_dim == 3
+REAL                          :: L_zeta(  0:NState,0:NInter,0:NInter,0:NInter)
+#endif
 REAL                          :: L_eta_zeta
 REAL                          :: xGP(0:NState),wBaryGP(0:NState)
 REAL                          :: Time
@@ -93,22 +95,24 @@ DO iElemNew=1,nElemsNew
     IF(NState.EQ.Nnew)THEN
       U(:,:,:,:,iElemNew)=UOld(:,:,:,:,iElemOld)
     ELSE
-      CALL ChangeBasis3D(nVar_State,NState,NNew,Vdm_GPNState_GPNNew,UOld(:,:,:,:,iElemOld),U(:,:,:,:,iElemNew))
+      CALL ChangeBasisVolume(nVar_State,NState,NNew,Vdm_GPNState_GPNNew,UOld(:,:,:,:,iElemOld),U(:,:,:,:,iElemNew))
     END IF
     CYCLE
   END IF
 
   ! rest
   ! Calculate basis function values for xiInter
-  DO kk=0,NInter; DO jj=0,NInter; DO ii=0,NInter
+  DO kk=0,ZDIM(NInter); DO jj=0,NInter; DO ii=0,NInter
     IF(.NOT.IPDone(ii,jj,kk,iElemNew)) CYCLE
     CALL LagrangeInterpolationPolys(xiInter(1,ii,jj,kk,iElemNew),NState,xGP,wBaryGP,L_xi(  :,ii,jj,kk))
     CALL LagrangeInterpolationPolys(xiInter(2,ii,jj,kk,iElemNew),NState,xGP,wBaryGP,L_eta( :,ii,jj,kk))
+#if PP_dim == 3
     CALL LagrangeInterpolationPolys(xiInter(3,ii,jj,kk,iElemNew),NState,xGP,wBaryGP,L_zeta(:,ii,jj,kk))
+#endif
   END DO; END DO; END DO
 
   ! Evaluate old solution at interpolation points and transform to new state polynomial degree and node type
-  DO kk=0,NInter; DO jj=0,NInter; DO ii=0,NInter
+  DO kk=0,ZDIM(NInter); DO jj=0,NInter; DO ii=0,NInter
     IF(.NOT.IPDone(ii,jj,kk,iElemNew))THEN
       ! If the interpolation point was not found, overwrite with reference state
       ! If no reference state was given, the program already aborted
@@ -118,16 +122,20 @@ DO iElemNew=1,nElemsNew
       Utmp(:,ii,jj,kk)=0.
     END IF
     iElemOld = InterToElem(ii,jj,kk,iElemNew)
-    DO k=0,NState
+    DO k=0,ZDIM(NState)
       DO j=0,NState
+#if PP_dim == 3
         L_eta_zeta=L_eta(j,ii,jj,kk)*L_zeta(k,ii,jj,kk)
+#else
+        L_eta_zeta=L_eta(j,ii,jj,kk)
+#endif
         DO i=0,NState
           Utmp(:,ii,jj,kk) = Utmp(:,ii,jj,kk) + UOld(:,i,j,k,iElemOld)*L_xi(i,ii,jj,kk)*L_eta_zeta
         END DO !i
       END DO !j
     END DO !k
   END DO; END DO; END DO
-  CALL ChangeBasis3D(nVar_State,NInter,NNew,Vdm_CLNInter_GPNNew,Utmp,U(:,:,:,:,iElemNew))
+  CALL ChangeBasisVolume(nVar_State,NInter,NNew,Vdm_CLNInter_GPNNew,Utmp,U(:,:,:,:,iElemNew))
 END DO
 !$OMP END DO
 !$OMP END PARALLEL
