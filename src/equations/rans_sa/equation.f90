73e2772da9b62e21b7c0906d0fa6319edcac9c02
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2021-02-18 17:05:07 +0100

    Fix posti visu, always deallocate HSize and mesh pointers

--- src/equations/rans_sa/equation.f90
+++ src/equations/rans_sa/equation.f90
@@ -1,9 +1,9 @@
 !=================================================================================================================================
-! Copyright (c) 2010-2016  Prof. Claus-Dieter Munz 
+! Copyright (c) 2010-2016  Prof. Claus-Dieter Munz
 ! This file is part of FLEXI, a high-order accurate framework for numerically solving PDEs with discontinuous Galerkin methods.
 ! For more information see https://www.flexi-project.org and https://nrg.iag.uni-stuttgart.de/
 !
-! FLEXI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+! FLEXI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 ! as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 !
 ! FLEXI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
@@ -47,7 +47,7 @@ PUBLIC:: GetPrimitiveStateSurface,GetConservativeStateSurface
 CONTAINS
 
 !==================================================================================================================================
-!> Define parameters 
+!> Define parameters
 !==================================================================================================================================
 SUBROUTINE DefineParametersEquation()
 ! MODULES
@@ -242,7 +242,7 @@ IF(nRefState .GT. 0)THEN
     RefStatePrim(1:5,i)  = RefStatePrimTmp(1:5)
 #if PP_dim==2
     IF(RefStatePrim(4,i).NE.0.) THEN
-      SWRITE(UNIT_StdOut,'(A)')' You are computing in 2D! RefStatePrim(4) will be set to zero!' 
+      SWRITE(UNIT_StdOut,'(A)')' You are computing in 2D! RefStatePrim(4) will be set to zero!'
       RefStatePrim(4,i)=0.
     END IF
 #endif
@@ -279,7 +279,7 @@ END SUBROUTINE InitEquation
 
 !==================================================================================================================================
 !> Converts conservative solution vector to primitive variables
-!> 
+!>
 !> Two possibilities for sides if using non-Lobatto node sets:
 !> 1. Convert U_master/slave to prims (used):
 !>    prims consistent to cons, but inconsistent to prim volume
@@ -289,7 +289,7 @@ END SUBROUTINE InitEquation
 !>    UPrim_master/slave consistent to UPrim, but inconsistent to U_master/slave
 !>    more expensive, communication and mortars required.
 !>    This version gives thermodynamically inconsistant states at sides.
-!> 
+!>
 !> TODO: Provide switch for these two versions.
 !==================================================================================================================================
 SUBROUTINE GetPrimitiveStateSurface(U_master,U_slave,UPrim_master,UPrim_slave)
@@ -355,8 +355,8 @@ REAL,INTENT(IN)    :: UPrim_master(PP_nVarPrim,0:PP_N,0:PP_NZ,1:nSides) !< primi
 REAL,INTENT(IN)    :: UPrim_slave( PP_nVarPrim,0:PP_N,0:PP_NZ,1:nSides) !< primitive solution on slave sides
 REAL,INTENT(OUT)   :: U_master(        PP_nVar,0:PP_N,0:PP_NZ,1:nSides) !< conservative solution on master sides
 REAL,INTENT(OUT)   :: U_slave(         PP_nVar,0:PP_N,0:PP_NZ,1:nSides) !< conservative solution on slave sides
-INTEGER,INTENT(IN) :: mask_master(1:nSides)                            !< mask: only convert solution if mask(SideID) == mask_ref 
-INTEGER,INTENT(IN) :: mask_slave (1:nSides)                            !< mask: only convert solution if mask(SideID) == mask_ref 
+INTEGER,INTENT(IN) :: mask_master(1:nSides)                            !< mask: only convert solution if mask(SideID) == mask_ref
+INTEGER,INTENT(IN) :: mask_slave (1:nSides)                            !< mask: only convert solution if mask(SideID) == mask_ref
 INTEGER,INTENT(IN) :: mask_ref                                         !< reference value for mask comparison
 !----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
