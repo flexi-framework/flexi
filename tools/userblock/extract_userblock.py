2a9b4d76849cdd6b0d9c4a8d9876190b6a9b0a92
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2022-07-21 12:59:55 +0200

    Make userblock python tools compatible with Python3

--- tools/userblock/extract_userblock.py
+++ tools/userblock/extract_userblock.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 #************************************************************************************
 #
-# Author:       Matthias Sonntag
-# Institution:  Inst. of Aero- and Gasdynamics, University of Stuttgart
-# Date:         14.07.2016
-#
 # Description:  This script contains routines to extract userblock data from an HDF5
 #               file containing git revision information, eventuelly a patch to
 #               the Git remote and the ini (parameter) file used for the computation.
@@ -17,59 +13,77 @@ import subprocess
 # extract userblock from HDF5 state file
 def get_userblock(filename,userblock) :
     linesread = 0
-    HDFfound = False
-    f = open(filename, 'r')
-    while True :
-      line = f.readline()
-      linesread = linesread + 1
+    HDFfound  = False
 
-      if line == "" :
-        break
+    # Open the file with UTF-8 encoding to read the string data
+    with open(filename, 'r', encoding='utf-8', errors='ignore') as f :
+        while True :
+            line      = f.readline()
+            linesread = linesread + 1
 
-      if linesread == 1 and not line.startswith('{[(') :
-        print 'Error: HDF5 state file %s contains no userblock.' % filename
-        exit(1)
+            # Exit on EOF
+            if line == "" :
+                break
 
-      if line.startswith('{[( END USERBLOCK )]}') :
-        break
+            # Exit on userblock not exist
+            if linesread == 1 and not line.startswith('{[(') :
+                print('Error: HDF5 state file {:s} contains no userblock.'.format(filename))
+                exit(1)
 
-      # check for HDF identifier (here the original HDF state file begins)
-      for i in range(len(line)) :
-          c = line[i]
-          if ord(c) == 0 : continue
-          if ord(c) == 137 : 
-              if line[i+1:i+4] == 'HDF' : HDFfound = True
-      if HDFfound :
-        break
+            # Exit on end of userblock
+            if line.startswith('{[( END USERBLOCK )]}') :
+                break
 
-      # check for compressed data
-      if line.startswith('{[( COMPRESSED )]}') :
-        try:
-          filenamec=f.readline()      #size in bytes
-          filenametar=filenamec.strip() + ".tar.xz"
-          filesize=int(f.readline()) #size in bytes
-          userblock_compressed=f.read(filesize)
-        except:
-          print 'Error: Could not extract compressed data.'
-          exit(1)
-        fc = open(filenametar, 'w')
-        fc.write(userblock_compressed)
-        fc.close()
-        try:
-          p = subprocess.call("tar -xJf " + filenametar,shell=True)
-        except:
-          print 'Error while extracting userblock data.'
-          exit(1)
+            # Check for HDF identifier (here the original HDF state file begins)
+            for i in range(len(line)) :
+                c = line[i]
+                if ord(c) == 0   : continue
+                if ord(c) == 137 :
+                    if line[i+1:i+4] == 'HDF' : HDFfound = True
+            if HDFfound :
+                break
 
-        try:
-          userblock = get_userblock(filenamec.strip(),userblock)
-        except:
-          print 'Error while reading compressed userblock data.'
-          exit(1)
-        continue
+            # Check for compressed data
+            if line.startswith('{[( COMPRESSED )]}') :
+                filenamec   = f.readline()      # name of userblock file
+                filenametar = filenamec.strip() + ".tar.xz"
+                filesize    = int(f.readline()) # compressed size  in bytes
+                fileposc    = f.tell()          # current position in bytes
 
-      # everything ok
-      userblock = userblock + line
+                # Jump to the end of the compressed data
+                f.seek(fileposc + filesize)
+
+                # Open the compressed file as binary
+                with open(filename, 'rb') as fc :
+                    # Jump to the correct position
+                    fc.seek(fileposc)
+                    try :
+                        userblock_compressed = fc.read(filesize)
+                    except :
+                        print('Error: Could not extract compressed data.')
+                        exit(1)
+                    # Write the compressed data
+                    fc = open(filenametar, 'wb')
+                    fc.write(userblock_compressed)
+                    fc.close()
+
+                    # Extract the compressed data
+                    try :
+                        p = subprocess.call("tar -xJf " + filenametar,shell=True)
+                    except :
+                        print('Error while extracting userblock data.')
+                        exit(1)
+
+                    # Read the compressed data
+                    try :
+                        userblock = get_userblock(filenamec.strip(),userblock)
+                    except :
+                        print('Error while reading compressed userblock data.')
+                        exit(1)
+                    continue
+
+            # everything ok
+            userblock = userblock + line
 
     return userblock
 
@@ -79,21 +93,20 @@ def print_all_parts(userblock) :
         # try if line contains a part identifier: {[( IDENTIFIER )]}
         try :
             if not line.startswith("{[(") : continue
-            identifier = line.split("{[(")[1].split(")]}")[0] 
+            identifier = line.split("{[(")[1].split(")]}")[0]
             if "END USERBLOCK" in identifier : break
-            # print identifier
-            print identifier
+            print(identifier)
         except :
             continue
 
 def get_part(userblock,part) :
-    ret = "" 
+    ret = ""
     output = False
     for line in userblock.split('\n') :
         # try if line contains a part identifier: {[( IDENTIFIER )]}
         try :
             if line.startswith("{[(") :
-                identifier = line.split("{[(")[1].split(")]}")[0] 
+                identifier = line.split("{[(")[1].split(")]}")[0]
                 # if identifier is found -> start output
                 if part in identifier :
                     output = True
@@ -107,32 +120,30 @@ def get_part(userblock,part) :
         if output : ret = ret + line + "\n"
     return ret
 
-
 # output a specific part of the userblock
 def print_part(userblock,part) :
     tmp = get_part(userblock,part)
     if tmp :
-       print tmp,
+        print(tmp)
 
 # output whole userblock
 def print_all(userblock) :
     for line in userblock.split('\n') :
-        print line
+        print(line)
 
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description='Extract information from userblock of a HDF5-state file.')
-    parser.add_argument('-s', '--show', action='store_true', help='show all available parts of the userblock')
-    parser.add_argument('-p', '--part', help='indicates which part of the userblock should be extracted')
-    parser.add_argument('-a', '--all', help='extract complete userblock', action='store_true')
-    parser.add_argument('filename', type=str, help='filename of hdf5 file')
+    parser.add_argument('-s'       , '--show' , action='store_true' , help='show all available parts of the userblock')
+    parser.add_argument('-p'       , '--part' ,                       help='indicates which part of the userblock should be extracted')
+    parser.add_argument('-a'       , '--all'  , action='store_true' , help='extract complete userblock')
+    parser.add_argument('filename' ,            type=str            , help='filename of hdf5 file')
 
     args = parser.parse_args()
 
-    userblock = get_userblock(args.filename,'') 
+    userblock = get_userblock(args.filename,'')
     if args.show :
         print_all_parts(userblock)
     if args.part :
         print_part(userblock,args.part)
     if args.all :
-       print_all(userblock)
+        print_all(userblock)
