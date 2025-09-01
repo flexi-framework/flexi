997c5034bb796a3beebd1013a22796b1ff828f49
Daniel Appel <daniel.appel@iag.uni-stuttgart.de>
2025-09-01 13:45:00 +0200

    do not use deprecated GOLD linker if LD causes segfaults - issue could be tracked down to LTO linker plugin up to LD version 2.35.2, i.e. disable LTO for these versions

--- cmake/SetBuildType.cmake
+++ cmake/SetBuildType.cmake
@@ -42,30 +42,34 @@ ELSE()
       MARK_AS_ADVANCED(FORCE CMAKE_GCC_AR)
       MARK_AS_ADVANCED(FORCE CMAKE_GCC_NM)
       MARK_AS_ADVANCED(FORCE CMAKE_GCC_RANLIB)
-      # Do not use the standard CMake LTO option for GNU (-flto -fno-fat-lto-objects), as it does not allow speed-up during linking
-      IF( CMAKE_GCC_AR AND CMAKE_GCC_NM AND CMAKE_GCC_RANLIB )
-        MESSAGE(STATUS "Found GCC binutils wrappers for LTO. Enabling LTO linker plugin.")
-        # Do not use the standard CMake LTO option for GNU (-flto -fno-fat-lto-objects), as it does not allow speed-up during linking
-        SET(CMAKE_INTERPROCEDURAL_OPTIMIZATION FALSE)
-        # Static libraries require either fat LTO objects (increases compilation time) or the use of linker plugins (per default enabled); the jobserver option reduces linking time
-        # More information at: https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html#Optimize-Options
-        IF (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
-          # GCC10 introduced the -flto=auto option, https://gcc.gnu.org/legacy-ml/gcc-patches/2019-07/msg01488.html
-          SET(CMAKE_Fortran_FLAGS  "${CMAKE_Fortran_FLAGS} -flto=auto -fuse-linker-plugin")
+      # Determine version of LD linker, since LTO linker plugins prior to version 2.35.2 as well as standard IPO cause segfaults
+      EXECUTE_PROCESS(COMMAND ld --version COMMAND grep "^GNU ld" COMMAND sed "s/^.* //g" OUTPUT_VARIABLE GNU_LD_VERSION)
+      STRING(STRIP "${GNU_LD_VERSION}" GNU_LD_VERSION)
+      IF( ${GNU_LD_VERSION} VERSION_GREATER "2.35.2" )
+        IF( CMAKE_GCC_AR AND CMAKE_GCC_NM AND CMAKE_GCC_RANLIB )
+          MESSAGE(STATUS "Found GCC binutils wrappers for LTO. Enabling LTO linker plugin.")
+          # Do not use the standard CMake LTO option for GNU (-flto -fno-fat-lto-objects), as it does not allow speed-up during linking
+          SET(CMAKE_INTERPROCEDURAL_OPTIMIZATION FALSE)
+          # Static libraries require either fat LTO objects (increases compilation time) or the use of linker plugins (per default enabled); the jobserver option reduces linking time
+          # More information at: https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html#Optimize-Options
+          IF (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
+            # GCC10 introduced the -flto=auto option, https://gcc.gnu.org/legacy-ml/gcc-patches/2019-07/msg01488.html
+            SET(CMAKE_Fortran_FLAGS  "${CMAKE_Fortran_FLAGS} -flto=auto -fuse-linker-plugin")
+          ELSE()
+            # Otherwise we must rely on hardcoded GCC jobserver. If it is not available, we will get warnings in the console but it still works, albeit in serial mode
+            SET(CMAKE_Fortran_FLAGS  "${CMAKE_Fortran_FLAGS} -flto=jobserver -fuse-linker-plugin")
+          ENDIF()
+          # Use the GCC wrapper for the complete toolchain to provide the path to the linker plugin (this might be the problem with using the CMAKE IPO)
+          SET(CMAKE_AR     "${CMAKE_GCC_AR}"     CACHE FILEPATH "" FORCE)
+          SET(CMAKE_NM     "${CMAKE_GCC_NM}"     CACHE FILEPATH "" FORCE)
+          SET(CMAKE_RANLIB "${CMAKE_GCC_RANLIB}" CACHE FILEPATH "" FORCE)
+          MARK_AS_ADVANCED(FORCE CMAKE_AR)
+          MARK_AS_ADVANCED(FORCE CMAKE_NM)
+          MARK_AS_ADVANCED(FORCE CMAKE_RANLIB)
         ELSE()
-          # Otherwise we must rely on hardcoded GCC jobserver. If it is not available, we will get warnings in the console but it still works, albeit in serial mode
-          SET(CMAKE_Fortran_FLAGS  "${CMAKE_Fortran_FLAGS} -flto=jobserver -fuse-linker-plugin")
+          MESSAGE(WARNING "GCC indicates LTO support, but binutils wrappers could not be found. Disabling LTO linker plugin." )
+          SET(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)  # Fall back to standard IPO
         ENDIF()
-        # Use the GCC wrapper for the complete toolchain to provide the path to the linker plugin (this might be the problem with using the CMAKE IPO)
-        SET(CMAKE_AR     "${CMAKE_GCC_AR}"     CACHE FILEPATH "" FORCE)
-        SET(CMAKE_NM     "${CMAKE_GCC_NM}"     CACHE FILEPATH "" FORCE)
-        SET(CMAKE_RANLIB "${CMAKE_GCC_RANLIB}" CACHE FILEPATH "" FORCE)
-        MARK_AS_ADVANCED(FORCE CMAKE_AR)
-        MARK_AS_ADVANCED(FORCE CMAKE_NM)
-        MARK_AS_ADVANCED(FORCE CMAKE_RANLIB)
-      ELSE()
-        MESSAGE(WARNING "GCC indicates LTO support, but binutils wrappers could not be found. Disabling LTO linker plugin." )
-        SET(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)  # Fall back to standard IPO
       ENDIF()
     ELSE()
       SET(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)   # For non-GNU compilers, enable IPO globally
