b24fa0a08278597086d0f8fce3bdb2ccb34e4085
Daniel Appel <daniel.appel@iag.uni-stuttgart.de>
2026-06-17 16:01:04 +0200

    Fortitude fix: C161 variable inquiry ALLOCATED() and use in same logical expression

--- posti/visu/mappings.f90
+++ posti/visu/mappings.f90
@@ -154,8 +154,10 @@ END IF
 
 ! check if the distribution of DG/FV elements has changed
 changedFV_Elems=.TRUE.
-IF (ALLOCATED(FV_Elems_old).AND.(SIZE(FV_Elems_loc).EQ.SIZE(FV_Elems_old))) THEN
-  changedFV_Elems = .NOT.ALL(FV_Elems_loc.EQ.FV_Elems_old)
+IF (ALLOCATED(FV_Elems_old)) THEN
+  IF (SIZE(FV_Elems_loc).EQ.SIZE(FV_Elems_old)) THEN
+    changedFV_Elems = .NOT.ALL(FV_Elems_loc.EQ.FV_Elems_old)
+  END IF
 END IF
 SDEALLOCATE(FV_Elems_old)
 ALLOCATE(FV_Elems_old(1:nElems))
@@ -315,8 +317,10 @@ END DO
 
 ! check if any varnames changed
 changedVarNames = .TRUE.
-IF (ALLOCATED(mapAllVarsToSurfVisuVars_old).AND.(SIZE(mapAllVarsToSurfVisuVars).EQ.SIZE(mapAllVarsToSurfVisuVars_old))) THEN
-  changedVarNames = .NOT.ALL(mapAllVarsToSurfVisuVars.EQ.mapAllVarsToSurfVisuVars_old)
+IF (ALLOCATED(mapAllVarsToSurfVisuVars_old)) THEN
+  IF (SIZE(mapAllVarsToSurfVisuVars).EQ.SIZE(mapAllVarsToSurfVisuVars_old)) THEN
+    changedVarNames = .NOT.ALL(mapAllVarsToSurfVisuVars.EQ.mapAllVarsToSurfVisuVars_old)
+  END IF
 END IF
 SDEALLOCATE(mapAllVarsToSurfVisuVars_old)
 ALLOCATE(mapAllVarsToSurfVisuVars_old(1:nVarAll))
@@ -367,9 +371,11 @@ doSurfVisu = nBCNamesVisu.GT.0
 
 ! check if any boundary changed
 changedBCnames = .TRUE.
-IF (ALLOCATED(mapAllBCNamesToVisuBCNames_old).AND.(SIZE(mapAllBCNamesToVisuBCNames).EQ.SIZE(mapAllBCNamesToVisuBCNames_old))) THEN
-  changedBCnames = .NOT.ALL(mapAllBCNamesToVisuBCNames.EQ.mapAllBCNamesToVisuBCNames_old)
-  IF (ALL(mapAllBCNamesToVisuBCNames_old.EQ.0) .AND. changedBCNames) changedStateFile = .TRUE.
+IF (ALLOCATED(mapAllBCNamesToVisuBCNames_old)) THEN
+  IF (SIZE(mapAllBCNamesToVisuBCNames).EQ.SIZE(mapAllBCNamesToVisuBCNames_old)) THEN
+    changedBCnames = .NOT.ALL(mapAllBCNamesToVisuBCNames.EQ.mapAllBCNamesToVisuBCNames_old)
+    IF (ALL(mapAllBCNamesToVisuBCNames_old.EQ.0) .AND. changedBCNames) changedStateFile = .TRUE.
+  END IF
 END IF
 SDEALLOCATE(mapAllBCNamesToVisuBCNames_old)
 ALLOCATE(mapAllBCNamesToVisuBCNames_old(1:nBCNamesAll))
